9680cd938106c96e142b414228a30bacf8354a75
Jacob Kjeldahl <jkj@lenio.dk>
2009-01-28 16:29:15 +0100

    Added file link to reek report

--- lib/metric_fu/reek.rb
+++ lib/metric_fu/reek.rb
@@ -11,7 +11,15 @@ module MetricFu
       files_to_reek = MetricFu.reek[:dirs_to_reek].map{|dir| Dir[File.join(dir, "**/*.rb")] }
       output = `reek #{files_to_reek.join(" ")}`
       @matches = output.chomp.split("\n\n").map{|m| m.split("\n") }
+      @filenames = extract_filenames(@matches.map {|m| m.first})
     end
 
+    private
+    def extract_filenames(names)
+      names.map do |name|
+        m = name.match(/^"(.*)"/)
+        m.size == 2 ? m[1] : nil
+      end
+    end
   end
 end
